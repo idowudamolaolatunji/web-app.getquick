98bb031f3da00614c4d58915bec8a4b5c8269da4
idowudamolaolatunji <idowudamolaolatunji@gmail.com>
2024-09-24 18:40:36 +0100

    committing changes

--- src/pages/home/homeComponents/Overview.jsx
+++ src/pages/home/homeComponents/Overview.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import OverviewChart from './OverviewChart'
 
 
 function Overview() {
   return (
     <div>
       <p>Overview</p>
+
+      {/* <OverviewChart /> */}
     </div>
   )
 }
---
+++ src/pages/home/homeComponents/OverviewChart.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function OverviewChart() {
+  return (
+    <div>OverviewChart</div>
+  )
+}
+
+export default OverviewChart

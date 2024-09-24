0293bc5b3b4f7d90719dbe827631f7dc79dbba01
idowudamolaolatunji <idowudamolaolatunji@gmail.com>
2024-09-25 00:12:44 +0100

    modified chart

--- src/pages/home/homeComponents/OverviewChart.jsx
+++ src/pages/home/homeComponents/OverviewChart.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactApexChart from 'react-apexcharts';
+import { useWindowSize } from 'react-use';
 
 function OverviewChart() {
+  const { width } = useWindowSize();
   const series = [
-    { name: 'Total Sales', data: [440, 550, 410, 64, 202, 403, 21, 0, 0, 0, 0, 0], color: '#655DBB' },
+    { name: 'Total Sales', data: [600, 550, 410, 64, 202, 403, 21, 0, 0, 0, 0, 0], color: '#655DBB' },
     { name: 'Online Sales', data: ['303', 302, 303, '520', 103, 44, 32, 0, 0, 0, 0, 0], color: '#BFACE2' },
-    { name: 'Ofline Sales', data: [300, 103, 32, 120, 301, 140, 13, 0, 0, 0, 0, 0], color: '#A084DC' },
+    { name: 'Offline Sales', data: [300, 103, 32, 120, 301, 140, 13, 0, 0, 0, 0, 0], color: '#A084DC' },
   ];
 
   const options = {
@@ -13,6 +15,11 @@ function OverviewChart() {
       type: 'bar',
       toolbar: {
         show: false,
+      },
+      font: {
+        family: 'Raleway, sans-serif',
+        size: '20px',
+        color: '#333',
       },
     },
     plotOptions: {
@@ -29,8 +36,18 @@ function OverviewChart() {
       colors: ['#fff'],
     },
     tooltip: {
+      enabled: width < 600 ? true : false,
       shared: true,
+      followCursor: true,
       intersect: false,
+      style: {
+        fontSize: '12px',
+        fontFamily: 'Raleway, sans-serif',
+        color: '#333',
+      },
+      onDatasetHover: {
+        highlightDataSeries: true,
+      },
     },
     grid: {
       borderColor: '#f1f1f1',
@@ -45,10 +62,36 @@ function OverviewChart() {
         },
       },
     },
-  
     xaxis: {
-      categories: ['Jan', 'Feb', 'Mar', "Apr", 'May', 'Jun', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+      categories: ['Jan', 'Feb', 'Mar', "Apr", 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+      labels: {
+        style: {
+          fontSize: '9.2px',
+          fontWeight: 500,
+          fontFamily: 'inherit',
+          colors: '#444',
+        },
+      },
     },
+    yaxis: {
+      labels: {
+        style: {
+          fontSize: '10px',
+          fontWeight: 500,
+          fontFamily: 'inherit',
+          colors: '#444',
+        },
+        formatter: (val) => {
+          if (val >= 1000000) {
+            return '₦' + (val / 1000000).toFixed(val % 1000000 === 0 ? 0 : 1) + 'M';
+          } else if (val >= 1000) {
+            return '₦' + (val / 1000).toFixed(val % 1000 === 0 ? 0 : 1) + 'k';
+          } else {
+            return '₦' + val.toFixed(0);
+          }        
+        },
+      },
+    }
   };
 
   return (

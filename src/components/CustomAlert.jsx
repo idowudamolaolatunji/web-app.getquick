08ddcfec898ec7bd48e64643ed31de0ca7cd0db4
idowudamolaolatunji <idowudamolaolatunji@gmail.com>
2024-09-29 12:23:29 +0100

    modified some more style

--- src/components/CustomAlert.jsx
+++ src/components/CustomAlert.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Alert from '@mui/material/Alert';
 import { AlertTitle, Snackbar } from '@mui/material';
 
-function CustomAlert({ type, message, duration=5000 }) {
+function CustomAlert({ type, message, duration=5000, style="filled" }) {
     const [open, setOpen] = useState(true);
     // success, info, warning, error
 
@@ -19,6 +19,7 @@ function CustomAlert({ type, message, duration=5000 }) {
             <Alert
                 onClose={handleClose}
                 severity={type}
+                variant={style}
                 sx={{ width: '100%', fontFamily: 'inherit', fontSize: '1.4rem', fontWeight: 500, display: 'flex', alignItems: 'center' }}
             >
                 {message}
